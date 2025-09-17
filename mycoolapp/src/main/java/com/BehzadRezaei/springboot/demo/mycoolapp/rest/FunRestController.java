3a9adf35d514291782838c4d02d97f273712d92f
Behzad Rezaei <Behrezaie@gmail.com>
2025-09-16 22:04:42 -0400

    Injected custom properties to be used in an endpoint.

--- mycoolapp/src/main/java/com/BehzadRezaei/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/BehzadRezaei/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,11 +2,25 @@ package com.BehzadRezaei.springboot.demo.mycoolapp.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class FunRestController {
-    // expose "/" that returns "Hello World"
 
+    // Injecting values from application.properties
+    @Value("${country.name}")
+    private String countryName;
+    
+    @Value("${country.province}")
+    private String countryProvince;
+
+    // expose new endpoint for "/country"
+    @GetMapping("/country")
+    public String getCountryInfo() {
+        return "Country: " + countryName + ", Province: " + countryProvince;
+    }
+
+    // expose "/" that returns "Hello World"
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
