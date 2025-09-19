f99ecf49b385f6a22ed9d63709edba53f2966ae7
Behzad Rezaei <Behrezaie@gmail.com>
2025-09-18 21:11:48 -0400

    Added @Primary annotation to practice it instead of @Qualifier.

--- 02-spring-boot-spring-core/src/main/java/com/BehzadRezaei/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/src/main/java/com/BehzadRezaei/springcoredemo/common/CricketCoach.java
@@ -1,5 +1,6 @@
 package com.BehzadRezaei.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 // Component annotation registers this class as a Spring bean, 
@@ -8,6 +9,11 @@ import org.springframework.stereotype.Component;
 // Because we have only one implementation of Coach interface (CricketCoach).
 // If we had multiple implementations, we would need to use @Qualifier annotation to specify which one to use.
 @Component
+// Primary annotation tells Spring to use this bean when there are multiple beans of the same type.
+// However, in this case, we are using @Qualifier in the controller to specify which bean to use.
+// and Qualifier has higher priority than Primary. therefore, since we have used both, Spring will use the bean specified in Qualifier.
+// We can't have multiple beans with @Primary annotation.
+@Primary
 public class CricketCoach implements Coach {
 
     @Override
