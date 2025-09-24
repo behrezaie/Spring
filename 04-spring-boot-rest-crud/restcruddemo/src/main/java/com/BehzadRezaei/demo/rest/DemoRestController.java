0c7190eb52a945e2843c1a83b1adc74fe4e9ead2
Behzad Rezaei <Behrezaie@gmail.com>
2025-09-23 22:19:32 -0400

    Updated DemoRestController with some comments about the difference between the RequestMapping and GetMapping.

--- 04-spring-boot-rest-crud/restcruddemo/src/main/java/com/BehzadRezaei/demo/rest/DemoRestController.java
+++ 04-spring-boot-rest-crud/restcruddemo/src/main/java/com/BehzadRezaei/demo/rest/DemoRestController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+// RequestMapping is used to define a base URL for all endpoints in this controller
+// While GetMapping is used to define a specific endpoint for HTTP GET requests
+// So, basically GetMapping is a specialized version of RequestMapping for GET requests
+// and is equal to @RequestMapping(method = RequestMethod.GET)
 public class DemoRestController {
 
     // exposing "/hello" endpoint that returns "Hello World!"
