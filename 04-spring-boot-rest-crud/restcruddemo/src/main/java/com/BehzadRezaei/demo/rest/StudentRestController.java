dd34e14f586765323315a1dd4b0310a340248198
Behzad Rezaei <Behrezaie@gmail.com>
2025-09-25 10:10:33 -0400

    Added @PathVariabel to an endpoint to practice having a dynamic endpoint.

--- 04-spring-boot-rest-crud/restcruddemo/src/main/java/com/BehzadRezaei/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/restcruddemo/src/main/java/com/BehzadRezaei/demo/rest/StudentRestController.java
@@ -1,9 +1,13 @@
 package com.BehzadRezaei.demo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.BehzadRezaei.demo.entity.Student;
+
+import jakarta.annotation.PostConstruct;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -11,17 +15,38 @@ import java.util.ArrayList;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> theStudents;
 
-        // create list of students
-        List<Student> theStudents = new ArrayList<>();
+    // define @PostConstruct to load the student data ... only once!
+    // @PostConstruct is used to run the method once after the bean is created
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Pooya", "Rezaei"));
         theStudents.add(new Student("Sara", "Rezaei"));
         theStudents.add(new Student("Ali", "Rezaei"));
+    }
+
+    // define endpoint for "/students" - return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return theStudents;
+    }
+
+    // define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    // @PathVariable is used to extract the value from the URL
+    // and assign it to the method parameter
+    // e.g. /students/1 -> studentId = 1
+    public Student getStudent(@PathVariable int studentId) {
+
+        // check the studentId against list size
+        if ((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new RuntimeException("Student id not found - " + studentId);
+        }
+
+        return theStudents.get(studentId);
     }
 }
