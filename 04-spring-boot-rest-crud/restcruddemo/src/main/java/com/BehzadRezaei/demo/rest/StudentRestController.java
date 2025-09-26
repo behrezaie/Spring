1880562b18db6ca5a337785713638fd940f59f91
Behzad Rezaei <Behrezaie@gmail.com>
2025-09-25 21:34:43 -0400

    Removed some unused imports.

--- 04-spring-boot-rest-crud/restcruddemo/src/main/java/com/BehzadRezaei/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/restcruddemo/src/main/java/com/BehzadRezaei/demo/rest/StudentRestController.java
@@ -1,8 +1,5 @@
 package com.BehzadRezaei.demo.rest;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
