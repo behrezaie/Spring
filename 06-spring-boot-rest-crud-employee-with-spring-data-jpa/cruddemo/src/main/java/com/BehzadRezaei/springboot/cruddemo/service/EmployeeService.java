554d9c476681a06c1c93f6d76a5f831ae376515c
Behzad Rezaei <Behrezaie@gmail.com>
2025-09-30 19:20:20 -0400

    Initiated the Data JPA project.

---
+++ 06-spring-boot-rest-crud-employee-with-spring-data-jpa/cruddemo/src/main/java/com/BehzadRezaei/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.BehzadRezaei.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.BehzadRezaei.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
