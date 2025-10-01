5f3f242b4aafbf346804e9d6054d1ad56c72cee9
Behzad Rezaei <Behrezaie@gmail.com>
2025-09-30 21:13:56 -0400

    Added EmployeeRepository for Spring Data JPA.

---
+++ 06-spring-boot-rest-crud-employee-with-srping-data-jpa/src/main/java/com/BehzadRezaei/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.BehzadRezaei.springboot.cruddemo.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.BehzadRezaei.springboot.cruddemo.entity.Employee;
+import com.BehzadRezaei.springboot.cruddemo.dao.EmployeeRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+            employeeRepository = theEmployeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        // TODO Auto-generated method stub
+        return employeeRepository.findAll();
+    }
+    
+    @Override
+    public Employee findById(int theId) {
+        // TODO Auto-generated method stub
+        return employeeRepository.findById(theId)
+                .orElse(null);
+    }
+    
+    @Override
+    public Employee save(Employee theEmployee) {    
+        // TODO Auto-generated method stub
+        return employeeRepository.save(theEmployee);
+    }
+    
+    @Override
+    public void deleteById(int theId) {
+        // TODO Auto-generated method stub
+        employeeRepository.deleteById(theId);
+    }
+
+}
