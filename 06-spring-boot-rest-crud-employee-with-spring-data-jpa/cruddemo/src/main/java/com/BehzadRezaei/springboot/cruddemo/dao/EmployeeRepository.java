f436dd0d124637add6a2d9191aaa0866ea338ed7
Behzad Rezaei <Behrezaie@gmail.com>
2025-09-30 21:01:10 -0400

    Removed problematic folder to add it one more time.

--- 06-spring-boot-rest-crud-employee-with-spring-data-jpa/cruddemo/src/main/java/com/BehzadRezaei/springboot/cruddemo/dao/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.BehzadRezaei.springboot.cruddemo.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaReposiory<Employee, Integer> {
-
-    // that's it ... no need to write any code LOL!
-
-}
