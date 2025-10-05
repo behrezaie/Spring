8aad5cb60532e5563718e0858aa2a0dd43ee5a4c
Behzad Rezaei <Behrezaie@gmail.com>
2025-10-05 15:03:53 -0400

    Updated DemoSecurityConfig to use custom tables for user and roles information, in case we had our own tables in the database, and didn't want to use the naming conventions that Spring is looking for.

--- 08-spring-boot-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 08-spring-boot-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -18,7 +18,15 @@ public class DemoSecurityConfig {
     // add support for JDBC ... no more hardcoded users
     @Bean
     public JdbcUserDetailsManager userDetailsManager(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+
+        // define query to retrieve a roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
     }
     
     // we don't need this method anymore since we are using JDBC authentication
